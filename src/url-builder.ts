454f071d534523afdfde4632a20eca1a060c14b3
Luis <101060519+LuisFun@users.noreply.github.com>
2024-05-07 14:45:02 +0900

    fix: type allow undefined

--- src/url-builder.ts
+++ src/url-builder.ts
@@ -1,3 +1,6 @@
-export const restUrl = (ACCOUNT_ID: string) => `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/ai/run`
-export const gatewayUrl = (ACCOUNT_ID: string, GATEWAY_SLUG: string) =>
-  `https://gateway.ai.cloudflare.com/v1/${ACCOUNT_ID}/${GATEWAY_SLUG}/workers-ai`
+export const restUrl = (ACCOUNT_ID: string | undefined) =>
+  ACCOUNT_ID ? `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/ai/run` : undefined
+export const gatewayUrl = (ACCOUNT_ID: string | undefined, GATEWAY_SLUG: string | undefined) =>
+  ACCOUNT_ID && GATEWAY_SLUG
+    ? `https://gateway.ai.cloudflare.com/v1/${ACCOUNT_ID}/${GATEWAY_SLUG}/workers-ai`
+    : undefined
